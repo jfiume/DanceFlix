b294f464e911221e346d21e59d6367e6dbb041fd
Joe Fiume <joefiume88@gmail.com>
2017-07-18 12:04:45 -0700

    added app and root components

--- frontend/danceflix.jsx
+++ frontend/danceflix.jsx
@@ -1,9 +1,15 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configreStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to DanceFlix</h1>, root);
+  const store = configreStore();
+
+  // testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+  ReactDOM.render(<Root store={store} />, root);
 });
