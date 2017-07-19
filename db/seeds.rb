5f50fbc168b0c85b16731ca9774b0cfb15503a95
Joe Fiume <joefiume88@gmail.com>
2017-07-19 14:55:24 -0700

    updated seed db to destroy all previous data before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-demo_user = User.create!(username: "WillDavis@willdemos.com", password: "password")
+User.destroy_all
+demo_user = User.create!(username: "WillDavis@willdemos.net", password: "password")
