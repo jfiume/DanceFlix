86038540286949d727427ca1a304d55c7601e5be
Joe Fiume <joefiume88@gmail.com>
2017-07-19 17:51:09 -0700

    ajax requests working

--- frontend/danceflix.jsx
+++ frontend/danceflix.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import * as APIUtil from './util/session_api_util';
+import * as APIUtilSession from './util/session_api_util';
+import * as APIUtilVideos from './util/video_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -19,9 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // testing
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.login = APIUtil.login;
-  window.signup = APIUtil.signup;
-  window.logout = APIUtil.logout;
+  window.login = APIUtilSession.login;
+  window.signup = APIUtilSession.signup;
+  window.logout = APIUtilSession.logout;
+  window.fetchVideo = APIUtilVideos.fetchVideo;
+  window.fetchVideos = APIUtilVideos.fetchVideos;
+
 
   ReactDOM.render(<Root store={store} />, root);
 });
