361618eb960064bd5bf5a2bfda22a97c09da14c0
Joe Fiume <joefiume88@gmail.com>
2017-07-21 16:54:55 -0700

    all functionality working expect I want a redirect when I login

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -17,10 +17,7 @@ class VideoShow extends Component {
   }
 
   render() {
-    console.log("id " + parseInt(this.props.match.params.id));
-    console.log("videoId " + this.props.videoId);
     if (this.props.videoId === parseInt(this.props.match.params.id)) {
-      console.log("videoObject " + this.props.currentVideo.video);
       const video = this.props.currentVideo.video;
       return (
         <iframe className="current-video" height="500" width="500" src={video.video_url} frameBorder="0" allowFullScreen></iframe>
