ec8fec21a3d72c8a23cdbdb181e0a99edb18afa7
Joe Fiume <joefiume88@gmail.com>
2017-07-24 22:06:03 -0700

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,11 +28,74 @@ video2 = Video.create(
   year: 2012
 )
 
-video3 =Video.create(
+video3 = Video.create(
 video_url: "https://www.youtube.com/embed/Cj3AV92fJ90",
 image_url: "http://res.cloudinary.com/pancake/image/upload/c_thumb,h_240,w_180/v1500863213/maxresdefault_vcfbc3.jpg",
 description: "JABBAWOCKEEZ perform at the 2017 NBA Finals Championship Game halftime show",
 title: "JABBAWOCKEEZ at the NBA Finals 2017",
 genre: "breakance",
 year: 2017
+)
+
+video4 = Video.create(
+video_url: "https://www.youtube.com/embed/PizxUk3lG3c",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500957162/0_h4ntpm.jpg",
+description: "Derek Hough Dancing with the Macy's Stars of Dance-Jaimie Goodwin.",
+title: "Moving Room Dance",
+genre: "ballroom",
+year: "2013"
+)
+
+video5 = Video.create(
+video_url: "https://www.youtube.com/embed/SKDnPGD8CIw",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500957595/0-1_gwms3u.jpg",
+description: "La La Land - A Lovely Night",
+title: "La La Land - A Lovely Night",
+genre: "Jazz",
+year: 2016
+)
+
+video6 = Video.create(
+video_url: "https://www.youtube.com/embed/6lAKlYTQVKY",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500958070/0-2_slwrs6.jpg",
+description: "Take the Lead - Tango",
+title: "Take the Lead - Tango",
+genre: "Tango",
+year: 2006
+)
+
+video7 = Video.create(
+video_url: "https://www.youtube.com/embed/l9BbUqHrWFI",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500958241/0-3_pzysyr.jpg",
+description: "End of season at Kellermans
+nobody puts Baby in the corner (I've Had) The Time of My Life",
+title: "Time of My Life",
+genre: "Salsa",
+year: 1987
+)
+
+video8 = Video.create(
+video_url: "https://www.youtube.com/embed/NqGslEZ5I6c",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500958538/0-4_t366ug.jpg",
+description: "A city teenager moves to a small town where rock music and dancing have been banned,
+and his rebellious spirit shakes up the populace.",
+title: "Footloose",
+genre: "footloose dancing",
+year: 1984
+)
+
+video9 = Video.create(
+video_url: "https://www.youtube.com/embed/7oKPYe53h78",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500958755/0-5_v4ydct.jpg",
+description: "Grease is the word",
+title: "Grease - You're the one that I want",
+year: 1978
+)
+
+video10 = Video.create(
+video_url: "https://www.youtube.com/embed/sT2daisxdvA",
+image_url: "http://res.cloudinary.com/pancake/image/upload/v1500959013/0-6_fgrfrx.jpg",
+description: "A Brooklyn teenager feels his only chance to succeed is as the king of the disco floor.",
+title: "Saturday Night Fever",
+year: 1977
 )
