3357597814e365db5539ce840f8376d5489eb0e6
Joe Fiume <joefiume88@gmail.com>
2017-07-28 14:24:40 -0700

    still preparing for heroku

--- frontend/danceflix.jsx
+++ frontend/danceflix.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import * as APIUtilSession from './util/session_api_util';
-import * as APIUtilVideos from './util/video_api_util';
-import * as APIUtilViews from './util/view_api_util';
+// import * as APIUtilSession from './util/session_api_util';
+// import * as APIUtilVideos from './util/video_api_util';
+// import * as APIUtilViews from './util/view_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // testing
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = APIUtilSession.login;
-  window.signup = APIUtilSession.signup;
-  window.logout = APIUtilSession.logout;
-  window.fetchVideo = APIUtilVideos.fetchVideo;
-  window.fetchVideos = APIUtilVideos.fetchVideos;
-  window.fetchViewsByUser = APIUtilViews.fetchViewsByUser;
-  window.createView = APIUtilViews.createView;
-  window.addView = APIUtilViews.addView;
-  window.searchVideo = APIUtilVideos.searchVideo;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.login = APIUtilSession.login;
+  // window.signup = APIUtilSession.signup;
+  // window.logout = APIUtilSession.logout;
+  // window.fetchVideo = APIUtilVideos.fetchVideo;
+  // window.fetchVideos = APIUtilVideos.fetchVideos;
+  // window.fetchViewsByUser = APIUtilViews.fetchViewsByUser;
+  // window.createView = APIUtilViews.createView;
+  // window.addView = APIUtilViews.addView;
+  // window.searchVideo = APIUtilVideos.searchVideo;
 
 
   ReactDOM.render(<Root store={store} />, root);
