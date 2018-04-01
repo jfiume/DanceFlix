061846547268c6c286d96d89783b102823a69c45
Joe Fiume <joefiume88@gmail.com>
2018-03-31 17:09:48 -0700

    added more users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe Api::UsersController, type: :controller do
   describe 'POST #create' do
     context 'with invalid params' do
       it "validates the presence of the user's username and password" do
-        post :create, params: { user: {username: 'Batman@supermansucks.net', password: ''}}
+        post :create, params: { user: { username: 'Batman@supermansucks.net', password: '' }}
         expect(:errors).to be_present
       end
+    end
+
+    it 'validates that the password is at least 6 charcters long' do
+      post :create, params: { user: { username: 'Superman@batmansucks.org', password: 'short'}}
+      expect(:erros).to be_present
     end
   end
 end
