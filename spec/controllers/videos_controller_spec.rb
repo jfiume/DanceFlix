e3296591fbd7b44044008d0ba2f1382fa4ac6915
Joe Fiume <joefiume88@gmail.com>
2018-04-13 14:21:33 -0700

    added a user controller rspec test

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+#
+# RSpec.describe Api::VideosController, type: :controller do
+#   describe '#index' do
+#     context 'a query is passed to index' do
+#       it 'calls search_by_title method when passed a title query' do
+#         get "api/search", params: { title: true }
+#         expect(index).to receive(:search_by_title)
+#       end
+#     end
+#   end
+# end
